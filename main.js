ff4bbfca8b31509599b64deb9981cc65cded0708
ialmontedr0 <worldrdanthony@gmail.com>
2023-12-28 14:39:32 -0400

    28 12 2023 commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document
+/*document
   .querySelector(".hamburguesa-btn")
   .addEventListener("click", function () {
     document.querySelector(".navbar-header").classList.toggle("visible");
@@ -38,4 +38,67 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
 
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
+}*/
+
+// Selectores reutilizables
+const navbarHeader = document.querySelector(".navbar-header");
+const menuOverlay = document.querySelector(".menu-overlay");
+const themeToggle = document.getElementById('theme-toggle');
+const flecha = document.querySelector(".mi-flecha");
+const seccion = document.querySelector("#seccionDos");
+
+// Funciones
+const toggleVisibility = (element) => {
+  if (element) {
+    element.classList.toggle("visible");
+  }
+};
+
+const removeVisibility = (element) => {
+  if (element) {
+    element.classList.remove("visible");
+  }
+};
+
+const toggleDarkMode = () => {
+  document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+};
+
+const navigateTo = (url) => {
+  window.location.href = url;
+};
+
+const smoothScrollToSection = () => {
+  if (seccion) {
+    seccion.style.display = "block";
+    seccion.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
+// Eventos
+if (navbarHeader && menuOverlay) {
+  document.querySelector(".hamburguesa-btn").addEventListener("click", () => toggleVisibility(navbarHeader));
+  menuOverlay.addEventListener("click", () => {
+    removeVisibility(navbarHeader);
+    removeVisibility(menuOverlay);
+  });
 }
+
+if (themeToggle) {
+  themeToggle.addEventListener('click', toggleDarkMode);
+}
+
+if (flecha) {
+  flecha.addEventListener("click", smoothScrollToSection);
+}
+
+// Inicialización
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem('darkMode') === 'true') {
+    toggleDarkMode();
+  }
+});
+
+// Ejemplo de uso de navigateTo
+// navigateTo("/index.html");
